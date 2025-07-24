5c867e52bbbf875cc6f9323e4e752aedcbeb67d0
Omarespo-dev <omaresposito21@gmail.com>
2025-07-24 02:32:49 +0200

    added logic for explore more button TOGGLE

--- src/components/Layouts/Main/MySkills.jsx
+++ src/components/Layouts/Main/MySkills.jsx
@@ -1,13 +1,26 @@
 
 //importo dati learning
-import {skills ,learningSkill} from "../../../../data/Skills";
+import { useState } from "react";
+import { skills, learningSkill } from "../../../../data/Skills";
 
 
 
 export default function MySkills() {
+
+  //stato per tenere traccia del explore more per la show
+  const [show, setShow] = useState(false)
+
+  //prendo i primi quattro elementi dall arr
+  const takeFirst4 = skills.slice(0, 4)
+
+  //Prendo il restante dall arr
+  const takeLast5 = skills.slice(4)
+
+
+
   return (
     <div className=" flex flex-wrap justify-center">
-      
+
       {/* Skills button */}
       <div className="h-[100px]  w-[90%] flex justify-center p-[20px] mt-[50px]">
         <section className="h-[60px] w-[200px] flex justify-center items-center border-3 border-b-black shadow-2xl">
@@ -15,9 +28,9 @@ export default function MySkills() {
         </section>
       </div>
 
-      {/* Skills complete */}
-      <div className="  w-[55%] flex justify-center p-[20px] gap-[50px] flex-wrap">
-        {skills.map(skill => (
+      {/* Skills PRIME 4 FISSE*/}
+      <div className="w-[55%] flex justify-center p-[20px] gap-[50px] flex-wrap ">
+        {takeFirst4.map(skill => (
 
           <section className="group h-[135px] w-[135px] flex justify-center items-center border-2 border-b-black flex-col shadow-2xl/20 cursor-pointer hover:bg-zinc-900 duration-200 ease-in" key={skill.id}>
             {skill.img}
@@ -25,7 +38,29 @@ export default function MySkills() {
           </section>
         ))}
       </div>
-      
+
+      {/* Skills ULTIMI 5 CON LOGICA EXPLORE MORE*/}
+      <div className={show ? "w-[55%] flex justify-center p-[20px] gap-[50px] flex-wrap " : "hidden"} >
+        {takeLast5.map(skill => (
+
+          <section className="group h-[135px] w-[135px] flex justify-center items-center border-2 border-b-black flex-col shadow-2xl/20 cursor-pointer hover:bg-zinc-900 duration-200 ease-in" key={skill.id}>
+            {skill.img}
+            <h1 className="mt-[10px] group-hover:text-white">{skill.name}</h1>
+          </section>
+        ))}
+      </div>
+
+      {/* Skills learning button */}
+      <div className="h-[100px]  w-[90%] flex justify-center p-[20px] mt-[10px]">
+        <section
+          className=" group h-[60px] w-[200px] flex justify-center items-center border-3 border-b-black shadow-2xl
+        cursor-pointer hover:bg-zinc-900 duration-200 ease-in"
+
+          onClick={() => setShow(!show)}>   {/* TOGGLE BUTTON */}
+
+          <h1 className="font-bold text-[18px] group-hover:text-white"> {show ? "EXPLORE LESS" : "EXPLORE MORE"} </h1>
+        </section>
+      </div>
 
       {/* Skills learning button */}
       <div className="h-[100px]  w-[90%] flex justify-center p-[20px] mt-[50px]">
@@ -33,7 +68,7 @@ export default function MySkills() {
           <h1 className="font-bold text-[18px]">LEARNING:</h1>
         </section>
       </div>
-      
+
       {/* Skills complete */}
       <div className="  w-[55%] flex justify-center p-[20px] gap-[50px] flex-wrap ">
         {learningSkill.map(skill => (
